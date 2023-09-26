26891646dd6fff6ce5b81a4dc3b3af398dc334a4
nasvank7 <nasvank7@gmail.com>
2023-09-26 22:01:15 +0530

    env

--- backened/server/config/db.ts
+++ backened/server/config/db.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export const connect=async()=>{
     
-const MONGO_URL="mongodb+srv://nasvank7:NJT9fewxMC4VCQF1@socialmedia.mqmrbda.mongodb.net/socialmedia?retryWrites=true&w=majority"
+
     try {
-        await mongoose.connect(MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('connected to mongodb');
         
     } catch (error) {
