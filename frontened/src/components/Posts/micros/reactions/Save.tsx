70f85384713940817b312a58666771f009eb29e9
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-07-24 16:52:10 +0530

    Update Save.tsx

--- frontened/src/components/Posts/micros/reactions/Save.tsx
+++ frontened/src/components/Posts/micros/reactions/Save.tsx
@@ -1,23 +1,55 @@
-import React from "react";
-import { BiDownload } from "react-icons/bi";
+import React, { useEffect, useState } from "react";
 import { BsBookmarkPlus, BsBookmarkPlusFill } from "react-icons/bs";
+import { GetUsernameFromRedux } from "../../../../services/redux/UserinRedux";
 
-const Save = (props:any) => {
-  const { saved } = props;
+interface PostDetails {
+  _id: string;
+  userId: {
+    _id: string;
+    username: string;
+    image: string;
+  };
+  description: string;
+  likes: string[];
+  image: string;
+  saved: string[];
+  createdAt: string;
+}
+
+interface SaveProps {
+  saved: boolean;
+  postDetails: PostDetails;
+  onSave: (saved: boolean) => void;
+}
+
+const Save: React.FC<SaveProps> = ({ saved, postDetails, onSave }) => {
+
+  const userDetails = GetUsernameFromRedux();
+  const [isSaved, setIsSaved] = useState(saved);
+
+  useEffect(() => {
+    if (userDetails?._id) {
+      const myselfSaved = postDetails?.saved?.includes(userDetails?._id);
+      setIsSaved(myselfSaved);
+    }
+  }, [postDetails, userDetails]);
+
+  const handleClick = () => {
+    if (userDetails?._id) {
+      const newSavedState = !isSaved;
+      onSave(newSavedState);
+      setIsSaved(newSavedState);
+    }
+  };
+
   return (
-    <>
-      {saved ? (
-        <BsBookmarkPlusFill
-          color="green"
-          className="sm:w-6 md:w-8 lg:w-10 h-8 flex py-2  cursor-pointer "
-        />
+    <div onClick={handleClick} className="cursor-pointer flex items-center ">
+      {isSaved ? (
+        <BsBookmarkPlusFill color="green" className="sm:w-6 md:w-8 lg:w-10 h-10 flex " />
       ) : (
-        <BsBookmarkPlus
-          className="sm:w-6 md:w-8 lg:w-10 flex  cursor-pointer"
-          size={25}
-        />
+        <BsBookmarkPlus className="sm:w-6 md:w-8 lg:w-10 h-10 flex " />
       )}
-    </>
+    </div>
   );
 };
 
