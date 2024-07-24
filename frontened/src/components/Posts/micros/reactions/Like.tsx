a92a3b80e2cabf8ac4003d22423442f8f44f325a
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-07-24 16:51:51 +0530

    Update Like.tsx

--- frontened/src/components/Posts/micros/reactions/Like.tsx
+++ frontened/src/components/Posts/micros/reactions/Like.tsx
@@ -1,16 +1,54 @@
-import React from "react";
-import { FcLike } from "react-icons/fc";
+import React, { useEffect, useState } from "react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-const Like = (props: any) => {
-  const { liked } = props;
+import { GetUsernameFromRedux } from "../../../../services/redux/UserinRedux";
+
+interface PostDetails {
+  _id: string;
+  userId: {
+    _id: string;
+    username: string;
+    image: string;
+  };
+  description: string;
+  likes: string[];
+  image: string;
+  saved: string[];
+  createdAt: string;
+}
+
+interface LikeProps {
+  liked: boolean;
+  postDetails: PostDetails;
+  onLike: (liked: boolean) => void;
+}
+
+const Like: React.FC<LikeProps> = ({ liked, postDetails, onLike }) => {
+  const userDetails = GetUsernameFromRedux();
+  const [isLiked, setIsLiked] = useState(liked);
+
+  useEffect(() => {
+    if (userDetails?._id) {
+      const myselfLiked = postDetails?.likes?.includes(userDetails?._id);
+      setIsLiked(myselfLiked);
+    }
+  }, [postDetails, userDetails]);
+
+  const handleClick = () => {
+    if (userDetails?._id) {
+      const newLikedState = !isLiked;
+      onLike(newLikedState);
+      setIsLiked(newLikedState);
+    }
+  };
+
   return (
-    <>
-      {liked ? (
-        <AiFillHeart color="red" className="sm:w-6 md:w-8 lg:w-10 h-8 cursor-pointer" />
+    <div onClick={handleClick} className="cursor-pointer">
+      {isLiked ? (
+        <AiFillHeart color="red" className="sm:w-6 md:w-8 lg:w-10 h-8" />
       ) : (
-        <AiOutlineHeart className="sm:w-6 md:w-8 lg:w-10 h-8 cursor-pointer" />
+        <AiOutlineHeart className="sm:w-6 md:w-8 lg:w-10 h-8" />
       )}
-    </>
+    </div>
   );
 };
 
