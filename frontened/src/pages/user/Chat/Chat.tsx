71ef2b243b8edf0c1bdc21a3b672d6acf1d73484
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-07-29 16:11:25 +0530

    Update Chat.tsx

--- frontened/src/pages/user/Chat/Chat.tsx
+++ frontened/src/pages/user/Chat/Chat.tsx
@@ -7,14 +7,14 @@ import { TbMessages } from "react-icons/tb";
 const Chat = () => {
   return (
     <div className="h-screen w-full flex relative">
-      <div className="  border-r-2 border-l-2 border-opacity-75 w-1/4 ml-4 my-4    sticky">
+      <div className=" lg:border-r-2 lg:border-l-2   border-opacity-75 lg:w-1/5 ml-4 my-4   sticky">
         <Navbar />
       </div>
 
       {/* <div className="w-1.5/4 rounded-3xl shadow-md   border-opacity-75  ">
         <SearchUser />
       </div> */}
-      <div className="flex flex-col w-3/4  rounded-3xl border-r-2    border-opacity-75 border-l-2 justify-center items-center">
+      <div className="w-full lg:w-4/5   mr-4 my-4  border-opacity-75 overflow-y-auto scrollbar-hidden">
         <span className="">
           <TbMessages size={50} className="border rounded-2xl border-black" />
         </span>
