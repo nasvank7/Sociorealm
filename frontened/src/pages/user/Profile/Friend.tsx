56cb066aa2c8e2a4e29712e978d75149587cf4e1
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-07-29 17:09:47 +0530

    Update Friend.tsx

--- frontened/src/pages/user/Profile/Friend.tsx
+++ frontened/src/pages/user/Profile/Friend.tsx
@@ -7,12 +7,12 @@ const Friend = () => {
   return (
     <div className='h-screen w-full flex'>
       
-    <div className='border-l-4 border border-black border-opacity-75 w-1/5 ml-4 my-4   sticky'>
+    <div className='lg:border-r-2 lg:border-l-2   border-opacity-75 lg:w-1/5 ml-4 my-4   sticky '>
       <Navbar/>
     </div>
 
    
-    <div className='w-4/5   border mr-4 my-4  border-opacity-75 overflow-y-auto scrollbar-hidden'>
+    <div className='w-full lg:w-4/5   mr-4 my-4  border-opacity-75 overflow-y-auto scrollbar-hidden'>
      <FriendUSerProfile/>
     </div>
   </div>
