99840dd96436aef23ff4e2aa938f8f3843198f80
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-07-29 16:12:51 +0530

    Update NewPost.tsx

--- frontened/src/pages/user/Post/NewPost.tsx
+++ frontened/src/pages/user/Post/NewPost.tsx
@@ -4,17 +4,17 @@ import AddPost from '../../../components/Posts/AddPost'
 
 const NewPost = () => {
   return (
-    <div className='h-screen w-full flex'>
-      
-    <div className='border-l-4 border border-black border-opacity-75 w-1/5 ml-4 my-4  '>
-      <Navbar />
-    </div>
+    <div className='h-full w-full flex'>
 
-   
-    <div className='w-4/5   mr-4 my-4 border-black border-opacity-75 '>
-     <AddPost/>
+      <div className='lg:border-r-2 lg:border-l-2   border-opacity-75 lg:w-1/5 ml-4 my-4   sticky '>
+        <Navbar />
+      </div>
+
+
+      <div className='w-full lg:w-4/5   mr-4 my-4  border-opacity-75 overflow-y-auto scrollbar-hidden'>
+        <AddPost />
+      </div>
     </div>
-  </div>
   )
 }
 
