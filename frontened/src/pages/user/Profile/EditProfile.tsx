9f73d2e51090ab91ef49fe83a419cfd40ca428da
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-07-29 17:09:30 +0530

    Update EditProfile.tsx

--- frontened/src/pages/user/Profile/EditProfile.tsx
+++ frontened/src/pages/user/Profile/EditProfile.tsx
@@ -4,11 +4,11 @@ import EditUserDetails from "../../../components/Profile/EditUserDetails";
 const EditProfile = () => {
   return (
     <div className="h-screen w-full flex">
-      <div className="border-l-4 border border-black border-opacity-75 w-1/5 ml-4 my-4  ">
+      <div className="lg:border-r-2 lg:border-l-2   border-opacity-75 lg:w-1/5 ml-4 my-4   sticky ">
         <Navbar />
       </div>
 
-      <div className="w-4/5    mr-4 my-4 border-black border-opacity-75 ">
+      <div className="w-full lg:w-4/5   mr-4 my-4  border-opacity-75 overflow-y-auto scrollbar-hidden">
         <EditUserDetails />
       </div>
     </div>
